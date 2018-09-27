8d633aac961169a7206b485dd0e2e31d7a7e4940
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2018-09-26 23:09:21 -0400

    cleaned lines in pdf and added them into Guest object

--- src/main/java/automatedwarrantsearch/main/App.java
+++ src/main/java/automatedwarrantsearch/main/App.java
@@ -1,10 +1,14 @@
 package automatedwarrantsearch.main;
 
+import model.Guest;
 import util.parsepdf.ParsePDF;
 
 public class App {
+
+    static final String NAME_OF_THE_FILE = "C:\\Projects\\automatedwarrantsearch\\src\\main\\resources\\policereport.pdf";
+
     public static void main(String[] args) {
-        ParsePDF parsePDF = new ParsePDF("C:\\Projects\\automatedwarrantsearch\\src\\main\\resources\\Guest List.pdf");
+        ParsePDF parsePDF = new ParsePDF(NAME_OF_THE_FILE);
         parsePDF.readPDFFile();
     }
 }
