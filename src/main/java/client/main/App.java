2775ae9e1506ddff434120f9deda6e032cae44a4
Ranjith Kumar <ranjithkumarravikumar52@gmail.com>
2019-03-07 14:47:01 -0500

    fixed path issue

--- src/main/java/client/main/App.java
+++ src/main/java/client/main/App.java
@@ -4,7 +4,8 @@ import util.parsepdf.ParsePDF;
 
 public class App {
 
-    static final String NAME_OF_THE_FILE = "C:\\Projects\\client\\src\\main\\resources\\policereport.pdf";
+    //TODO relative path
+    static final String NAME_OF_THE_FILE = "C:\\Projects\\automated-warrant-search\\src\\main\\resources\\Guest List.pdf";
 
     public static void main(String[] args) {
         ParsePDF parsePDF = new ParsePDF(NAME_OF_THE_FILE);
